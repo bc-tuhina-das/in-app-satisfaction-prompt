e955e612a0acb9bf889994e2732c99e46d8a2635
bc-tuhina-das <tuhina.das@bigcommerce.com>
2024-06-20 11:23:27 -0500

    Initial Commit

---
+++ in-app-satisfaction-prompt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  in-app-satisfaction-prompt
+//
+//  Created by Tuhina Das on 6/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ in-app-satisfaction-prompt/in_app_satisfaction_promptApp.swift
@@ -0,0 +1,17 @@
+//
+//  in_app_satisfaction_promptApp.swift
+//  in-app-satisfaction-prompt
+//
+//  Created by Tuhina Das on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct in_app_satisfaction_promptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
